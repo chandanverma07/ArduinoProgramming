45a28a975fa6a114a642095ee90b587e3e1457d1
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-05-28 11:41:44 +0530

    Add files via upload

---
+++ Program1.1.ino
@@ -0,0 +1,18 @@
+void setup()
+{
+//Pin 13 Behave as Output
+pinMode(13, OUTPUT);
+}
+void loop()
+{
+//Transmits “1” From PIN13
+digitalWrite(13, HIGH);
+//wait for 100ms                                               
+delay(1000);
+//Transmits  “0” From PIN13
+digitalWrite(13, LOW);
+delay(1000);
+//wait for 100ms
+
+}
+
