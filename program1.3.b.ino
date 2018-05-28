a667aa72d1bd4200a0baf5a17d82896999ad7172
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-05-28 23:59:27 +0530

    Add files via upload

---
+++ program1.3.b.ino
@@ -0,0 +1,20 @@
+const int button = 2;   
+const int led =  13;
+int State = 0;
+
+void setup() {
+  pinMode(led, OUTPUT);      
+  pinMode(button, INPUT);     
+}
+
+void loop()
+{
+  State = digitalRead(button);
+  if (State == HIGH) {       
+    digitalWrite(led, HIGH);  
+  } 
+  else {
+    digitalWrite(led , LOW); 
+  }
+}
+
