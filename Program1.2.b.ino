7474ac554c2e4e60eeee7c6da1d8c6290228a057
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-05-28 11:45:23 +0530

    Add files via upload

---
+++ Program1.2.b.ino
@@ -0,0 +1,20 @@
+int i ;
+int pin[]={2,3,4,5,6,7};
+void setup()
+{
+for(i=0;i<8;i++)
+  {
+pinMode(pin[i],OUTPUT);
+  }
+}
+void loop()
+{ 
+  for(i=0;i<8;i++)
+  {
+digitalWrite(pin[i],HIGH);
+delay(500);
+digitalWrite(pin[i],LOW);
+ delay(500);
+  }
+}
+
