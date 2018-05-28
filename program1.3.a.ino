2f52b076c9e1775ddd0fbe07959ed55f5c8ec279
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-05-28 23:56:33 +0530

    Add files via upload

---
+++ program1.3.a.ino
@@ -0,0 +1,21 @@
+int inpin=7;
+int outpin=13;
+int value=0;
+void setup()
+{
+  pinMode(inpin,INPUT);
+  pinMode(outpin,OUTPUT);
+}
+void loop()
+{
+  value=digitalRead(inpin);
+  if(value==HIGH)
+  {
+    digitalWrite(outpin,HIGH);
+  }
+  else
+  {
+    digitalWrite(outpin,LOW);
+  }
+} 
+
