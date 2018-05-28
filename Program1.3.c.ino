edc1916b4748753aa7413aad19fe31a7229a2eb4
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-05-29 00:01:33 +0530

    Add files via upload

---
+++ Program1.3.c.ino
@@ -0,0 +1,23 @@
+int sw=8;
+int led=13;
+boolean lastb=LOW;
+boolean ledon=false;
+void setup()
+{
+  pinMode(sw,INPUT);
+  pinMode(led,OUTPUT);
+}
+void loop()
+{
+  if(digitalRead(sw) ==HIGH && lastb == LOW)
+  {
+  ledon=!ledon;
+  lastb=HIGH;
+}
+else
+{
+  lastb=digitalRead( sw);
+}
+digitalWrite(led, ledon);
+}
+
