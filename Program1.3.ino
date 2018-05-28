ee631b06897bf34d101eec66a010902c7d542882
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-05-28 19:25:19 +0530

    Add files via upload

---
+++ Program1.3.ino
@@ -0,0 +1,28 @@
+int redPin= 9;
+int greenPin = 10;
+int bluePin = 11;
+
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(redPin,HIGH)
+  digitalWrite(greenPin,LOW)
+  digitalWrite(bluePin,LOW)
+  delay(100)
+  digitalWrite(redPin,LOW)
+  digitalWrite(greenPin,HIGH)
+  digitalWrite(bluePin,LOW)
+  delay(1000)
+  digitalWrite(redPin,LOW)
+  digitalWrite(greenPin,LOW)
+  digitalWrite(bluePin,HIGH)
+  delay(1000)
+  
+
+}
+
+
