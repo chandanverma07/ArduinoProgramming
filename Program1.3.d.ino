83ad3b277a6d55b3877c9e6d73ecc155ddca7bfd
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-05-29 00:06:38 +0530

    Add files via upload

---
+++ Program1.3.d.ino
@@ -0,0 +1,37 @@
+int sw=8;
+int led=13;
+boolean lastb=LOW;
+boolean ledon=false;
+boolean currentb=LOW;
+void setup()
+{
+ pinMode(sw,INPUT);
+  pinMode(led,OUTPUT);
+}
+boolean debounce(boolean last)
+{
+  boolean current=digitalRead(sw);
+  if(last !=current)
+  {
+delay(5);
+ current=digitalRead(sw);
+  }
+  return current;
+  }
+
+
+void loop()
+{
+  currentb=debounce(lastb);
+  if(lastb  ==LOW && currentb == HIGH)
+  {
+  ledon=!ledon;
+}
+else
+{
+  lastb=currentb;
+}
+
+digitalWrite(led, ledon);
+}
+
