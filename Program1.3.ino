2521c0808f0735bd833ad2eaff3832a8d05d0cf9
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-05-28 19:13:21 +0530

    Add files via upload

---
+++ Program1.3.ino
@@ -0,0 +1,28 @@
+int redPin= 7;
+int greenPin = 6;
+int bluePin = 5;
+
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+}
+
+void loop() {
+  writingColor(255, 0, 0); // Red Color
+  delay(1000);
+  writingColor(0, 255, 0); // Green Color
+  delay(1000);
+  writingColor(0, 0, 255); // Blue Color
+  delay(1000);
+  writingColor(255, 255, 255); // White Color
+  delay(1000);
+ writingColor(170, 0, 255); // Purple Color
+  delay(1000);
+}
+
+void writingColor(int redValue, int greenValue, int blueValue) {
+  analogWrite(redPin, redValue);
+  analogWrite(greenPin, greenValue);
+  analogWrite(bluePin, blueValue);
+}
