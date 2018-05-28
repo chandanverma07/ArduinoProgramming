140d4732a12648af3ee1ad1fe274a6e5486a6fca
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-05-28 11:47:43 +0530

    Add files via upload

---
+++ Program1.2.a.ino
@@ -0,0 +1,17 @@
+int timer = 1000;        
+void setup() {
+  for (int thisPin = 2; thisPin < 8; thisPin++)  
+ {
+ pinMode(thisPin, OUTPUT);      
+  }
+  }
+
+void loop() {
+  for (int thisPin = 2; thisPin < 8; thisPin++) { 
+    digitalWrite(thisPin, HIGH);   
+    delay(timer);                  
+   digitalWrite(thisPin, LOW);   
+   delay(timer); 
+  }
+  }
+
