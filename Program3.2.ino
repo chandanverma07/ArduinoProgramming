9e1f6ed96b9f0aee0d23a670332dc68b49e24296
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-05-29 18:40:54 +0530

    Add files via upload

---
+++ Program3.2.ino
@@ -0,0 +1,15 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(2,4,6,8,10,12);
+void setup() 
+{
+lcd.begin(16,2);
+lcd.print("chandan");
+}
+void loop()
+{
+  lcd.noBlink();
+  delay(3000);
+  lcd.blink();
+  delay(3000);
+}
+
