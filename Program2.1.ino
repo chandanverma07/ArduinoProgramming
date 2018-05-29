8d24554fbdcdd86bc0716f98cdfc344384de3997
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-05-29 11:39:15 +0530

    Add files via upload

---
+++ Program2.1.ino
@@ -0,0 +1,23 @@
+
+
+
+const int Sensor=2;
+
+void setup() {                
+
+  pinMode(13, OUTPUT);     
+ 
+  pinMode(Sensor,INPUT);
+}
+
+void loop() {
+  if(digitalRead(Sensor)==HIGH)     
+  {
+    digitalWrite(13, HIGH);   
+  }
+  else
+  {
+    digitalWrite(13, LOW);   
+  }
+  delay(100);             
+}
