8faea30d5e09770c12dfdce528abf7235ca2b2d3
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-05-29 15:11:54 +0530

    Add files via upload

---
+++ Program2.4.ino
@@ -0,0 +1,17 @@
+ int led = 9;          
+int brightness = 0;    
+int value = 5;  
+void setup() {
+  pinMode(led, OUTPUT);
+}
+void loop() {
+  analogWrite(led, brightness);
+  brightness = brightness + value;
+ 
+  if (brightness == 0 || brightness == 255) {
+    value=-value ;
+  }
+  delay(30);
+}
+
+
