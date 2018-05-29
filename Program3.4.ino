1197c72a076abb65d31621e1dcb83426cefd1324
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-05-29 18:50:05 +0530

    Add files via upload

---
+++ Program3.4.ino
@@ -0,0 +1,13 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(2,4,6,8,10,12);
+void setup() {
+  lcd.begin(16, 2);
+  lcd.print("Arduino");
+}
+void loop() {
+  lcd.noDisplay();
+  delay(500);
+  lcd.display();
+  delay(500);
+}
+
