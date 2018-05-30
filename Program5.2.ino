3bc13228a2a39857c308d3fcf56db00237e59090
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-05-30 16:44:26 +0530

    Add files via upload

---
+++ Program5.2.ino
@@ -0,0 +1,24 @@
+int a;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);
+}
+void loop()
+{
+  if (Serial.available())
+  {
+    a=Serial.read();
+   a=a-48;
+    Serial.print(a);
+    if(a==1)
+    {
+     Serial.println("yes"); 
+     digitalWrite(13,HIGH);
+    }
+else
+{
+  Serial.println("no");
+  digitalWrite(13,LOW);
+}}}
+
