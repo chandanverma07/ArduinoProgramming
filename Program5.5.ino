dbb10c12815cdff830031b8086a8f2fa81c69365
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-05-30 17:27:45 +0530

    Add files via upload

---
+++ Program5.5.ino
@@ -0,0 +1,17 @@
+const int lm_pin = A1;  /* LM35 O/P pin */
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int read_temp_val;
+  float temp_val;
+  read_temp_val = analogRead(A1);
+  temp_val = (read_temp_val * 4.88);
+  temp_val = (temp_val/10);
+  Serial.print("Temperature = ");
+  Serial.print(temp_val);
+  Serial.print(" Degree Celsius\n");
+  delay(1000);
+}
