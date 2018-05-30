32c1f34aac0159ee5ed9eb57bc7a3c22888c9e2f
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-05-30 17:27:02 +0530

    Add files via upload

---
+++ Program5.6.ino
@@ -0,0 +1,13 @@
+int value = 10;  
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(value);       
+  Serial.println(value, DEC);  
+  Serial.println(value, HEX);  
+  Serial.println(value, OCT);  
+  Serial.println(value, BIN);  
+}
+void loop() {  
+
+}
