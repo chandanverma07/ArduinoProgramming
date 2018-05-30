c264c0718004b2f6738747dbfb86974c382ebe0f
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-05-30 16:48:24 +0530

    Add files via upload

---
+++ Program5.3.ino
@@ -0,0 +1,20 @@
+int IR = 2;
+void setup() {
+ Serial.begin(9600);
+  pinMode(IR, INPUT);
+ }
+void loop() {
+ int IRstate = digitalRead(IR);
+  Serial.println(IRstate);
+  if(IRstate==1)
+  {
+    Serial.println("SENSOR IS ON");
+    delay(500);
+  }
+  else
+  {
+    Serial.println("SENSOR IS OFF");
+    delay(500);
+  }
+     delay(100); }
+
