9b2c8937c40a2fa648cc00be22444daad37ec026
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-05-30 19:26:01 +0530

    Add files via upload

---
+++ Program5.9.ino
@@ -0,0 +1,33 @@
+#include <LiquidCrystal.h> 
+LiquidCrystal lcd(2,4,6,8,10,12);
+const int trigPin = 9;
+const int echoPin = 11;
+long duration;
+int distanceCm, distanceInch;
+void setup() {
+lcd.begin(16,2); 
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);
+}
+
+void loop() {
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+duration = pulseIn(echoPin, HIGH);
+distanceCm= duration*0.034/2;
+distanceInch = duration*0.0133/2;
+lcd.setCursor(0,0); 
+lcd.print("Distance: ");
+lcd.print(distanceCm); 
+lcd.print(" cm");
+delay(10);
+lcd.setCursor(0,1);
+lcd.print("Distance: ");
+lcd.print(distanceInch);
+lcd.print(" inch");
+delay(10);
+}
+
