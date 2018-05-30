89ef401b6ce24df93c6ced72e256e6bdee3f362f
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-05-30 19:17:27 +0530

    Add files via upload

---
+++ Program5.8.ino
@@ -0,0 +1,24 @@
+#define trigPin 9
+#define echoPin 11
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+void loop() {
+  int duration, distance;
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  if (distance >= 200 || distance <= 0){
+    Serial.println("Out of range");
+  }
+  else {
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+  delay(500);
+}
+
