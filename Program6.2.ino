8733e60b5d563ae7dbf49e6df90ad480eb4ccf66
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-05-31 16:38:47 +0530

    Add files via upload

---
+++ Program6.2.ino
@@ -0,0 +1,17 @@
+const int buttonPin = 2;   
+const int ledPin =  13;    
+volatile int buttonState = 0;        
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  attachInterrupt(0, pin_ISR, CHANGE);
+}
+void loop()
+{
+}
+
+void pin_ISR() {
+  buttonState = digitalRead(buttonPin);
+  digitalWrite(ledPin, buttonState);
+}
+
