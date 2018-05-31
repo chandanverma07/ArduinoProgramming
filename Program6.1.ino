3c65ff9aeb0b761b3f68d4fbc45d7c33cc18c8ab
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-05-31 16:38:25 +0530

    Add files via upload

---
+++ Program6.1.ino
@@ -0,0 +1,16 @@
+const byte ledPin = 13;
+const byte interruptPin = 2;
+volatile byte state = LOW;
+void setup() 
+{
+pinMode(ledPin, OUTPUT);
+attachInterrupt(digitalPinToInterrupt(interruptPin), blink1, RISING);
+}
+void loop() 
+{
+digitalWrite(ledPin, state);
+}
+void blink1() {
+state =!state;
+} 
+
