90a187ca03f1220d32582fcb815c4e74c5dcd5b0
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-05-31 17:20:26 +0530

    Add files via upload

---
+++ Program6.3.ino
@@ -0,0 +1,17 @@
+int buzzer = 13;
+void setup()
+{
+  pinMode(buzzer, OUTPUT);   
+  digitalWrite(buzzer, LOW); 
+  attachInterrupt(0, alarmON, RISING); 
+}
+
+void loop()
+{   
+
+}
+
+void alarmON()
+{  
+  digitalWrite(buzzer, HIGH); 
+}
