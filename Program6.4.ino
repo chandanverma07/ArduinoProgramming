b808ec95efec772f5cc46d9e3a811a6008eea3a1
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-05-31 17:20:05 +0530

    Add files via upload

---
+++ Program6.4.ino
@@ -0,0 +1,24 @@
+int pirPin = 2; //digital 2
+int counter = 0;
+int state;
+int laststate = HIGH;
+
+void setup(){
+Serial.begin(9600);
+pinMode(pirPin, INPUT);
+}
+
+void loop(){
+    int state = digitalRead(pirPin);
+       if ( state != laststate) {
+                   
+       //was motion detected
+       counter=counter+1;
+       Serial.println(counter);
+      }
+       
+   
+    
+  laststate = state;
+    delay(0);
+  }
