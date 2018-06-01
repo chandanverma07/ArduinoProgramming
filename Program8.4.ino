5a404cac1c254db5ddb3aa9d55af94fc2d1b655a
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-06-02 02:34:10 +0530

    Add files via upload

---
+++ Program8.4.ino
@@ -0,0 +1,18 @@
+#include  <ESP8266WiFi.h>
+void  setup() { 
+  Serial.begin(115200);   
+  Serial.println();
+  WiFi.begin("chandan",  "1234568");
+  Serial.print("Connecting");  
+  while (WiFi.status()  !=  WL_CONNECTED)   
+  {       
+    delay(500);       
+    Serial.print(".");    
+    }   
+    Serial.println();
+    Serial.print("Connected,  IP  address:  ");   
+    Serial.println(WiFi.localIP());
+    }
+void  loop()  
+{}
+
