c52997d9c20afb76844ab105b1683829d600a581
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-06-02 01:40:43 +0530

    Add files via upload

---
+++ Program8.1.ino
@@ -0,0 +1,10 @@
+void setup() {
+  pinMode(D7, OUTPUT);     
+}
+void loop() {
+  digitalWrite(D7, LOW);                                                             
+  delay(1000);                     
+  digitalWrite(D7, HIGH);  
+  delay(2000);                     
+}
+
