14fd6da76205c0f4ba3ecc124ada1b6443d2fe8e
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-06-02 02:17:24 +0530

    Add files via upload

---
+++ Program8.3.ino
@@ -0,0 +1,21 @@
+#include <ESP8266WiFi.h>
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  WiFi.disconnect();
+  delay(1000);
+  Serial.println("Nearby networksd found");
+  Serial.println(WiFi.scanNetworks());
+  delay(500);
+  Serial.println("List of Surrounding Network SSID..");
+  int n=WiFi.scanNetworks();
+  for(int i=0;i<n;i++)
+  {
+    Serial.println(WiFi.SSID(i));
+  }
+  Serial.println();
+}
+void loop()
+{}
+
