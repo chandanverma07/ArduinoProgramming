51e322754cb77de9a1e7a915f6a0edb42a9cc1dc
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-06-01 14:58:42 +0530

    Add files via upload

---
+++ Program7.1.ino
@@ -0,0 +1,13 @@
+
+int relay = 10; 
+void setup ()
+{
+  pinMode (relay, OUTPUT);
+}
+void loop ()
+{
+  digitalWrite (relay, HIGH);
+  delay (1000);
+  digitalWrite (relay, LOW);
+  delay (1000);
+}
