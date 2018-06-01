df67bef393fbec57bccec2b6dd43cced96c08f53
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-06-01 15:09:28 +0530

    Add files via upload

---
+++ Program7.2.ino
@@ -0,0 +1,22 @@
+
+int relay = 7;
+void setup()
+{
+  pinMode(relay, OUTPUT);   
+  digitalWrite(relay, LOW);  
+  attachInterrupt(0, lightON, RISING); 
+}
+
+void loop()
+{   
+
+}
+
+void lightON()
+{ 
+  digitalWrite(relay, HIGH); 
+  for(int i = 0; i <= 3000; i++){ //50 Seconds Aprox. time 
+    delay(1000);
+  }
+  digitalWrite(relay, LOW); 
+ }
