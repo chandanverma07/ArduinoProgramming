a0a262803de2a72bcf96812fcb1b0d7fa8d41672
Verma Chandan <36379746+chandanverma07@users.noreply.github.com>
2018-06-02 13:06:14 +0530

    Add files via upload

---
+++ Program8.9.ino
@@ -0,0 +1,34 @@
+#include <ESP8266WiFi.h>
+WiFiServer server(80);
+void setup()
+{
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP("CHANDAN_NODEMCU","12345678");
+  server.begin();
+  Serial.begin(115200);
+  IPAddress HTTPS_ServerIP=WiFi.softAPIP();
+  Serial.println("Server Ip is: " );
+  Serial.println(HTTPS_ServerIP);
+  pinMode(D7,OUTPUT);
+  digitalWrite(D7,LOW);
+}
+void loop()
+{
+  WiFiClient client=server.available();
+  if(!client)
+  {
+    return;
+  }
+  Serial.println("Clint has connected");
+  String request=client.readStringUntil('\r');
+  Serial.println(request);
+  if(request.indexOf("/OFF")!=-1)
+  {
+    digitalWrite(D7,LOW);
+  }
+  else if(request.indexOf("/ON")!=-1)
+  {
+    digitalWrite(D7,HIGH);
+  }
+  }
+
